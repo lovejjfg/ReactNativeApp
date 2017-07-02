bb5e973267c0afc8c3fe304b772bc95343158dbd
joe <lovejjfg@gmail.com>
2017-07-02 11:20:23 +0800

    add DrawLayoutAndroid

--- index.android.js
+++ index.android.js
@@ -12,31 +12,42 @@ import {
   Text,
   Button,
   Alert,
-  View
+  View,
+  DrawerLayoutAndroid
 } from 'react-native';
 
 
 export default class ReactNativeApp extends Component {
   render() {
+   var navigationView = (
+            <View style={{flex: 1, backgroundColor: '#fff'}}>
+              <Text style={{margin: 10, fontSize: 15, textAlign: 'left'}}>I'm in the Drawer!</Text>
+            </View>
+          );
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
-       <Button
-         onPress={onPressLearnMore}
-         title="Learn More"
-         color="#841584"
-         accessibilityLabel="Learn more about this purple button"
-       />
-      </View>
+      <DrawerLayoutAndroid
+            drawerWidth={300}
+            drawerPosition={DrawerLayoutAndroid.positions.Left}
+            renderNavigationView={() => navigationView}>
+                <View style={styles.container}>
+                  <Text style={styles.welcome}>
+                    Welcome to React Native!
+                  </Text>
+                  <Text style={styles.instructions}>
+                    To get started, edit index.android.js
+                  </Text>
+                  <Text style={styles.instructions}>
+                    Double tap R on your keyboard to reload,{'\n'}
+                    Shake or press menu button for dev menu
+                  </Text>
+                 <Button
+                   onPress={onPressLearnMore}
+                   title="Learn More"
+                   color="#841584"
+                   accessibilityLabel="Learn more about this purple button"
+                 />
+                </View>
+       </DrawerLayoutAndroid>
     );
   }
 }
